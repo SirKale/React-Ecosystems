4b03fb4258cfeb3e76ff8d22bac71fd6b2c21e32
Caleb Carter <calebcarter@Ronalds-MBP.fios-router.home>
2024-09-19 18:13:11 -0400

    Ninth commit

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -2,10 +2,10 @@ import React from "react";
 import { connect } from "react-redux";
 import TodoListItem from './TodoListItem';
 import NewTodoForm from './NewTodoForm'; 
-import { removeTodo } from "./actions";
+import { removeTodo, completedTodo} from "./actions";
 import './TodoList.css'
 
-const TodoList = ({ todos = [], onRemovePressed }) => (
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => (
     <div className="list-wrapper">
         <NewTodoForm />
         {todos.map(todo => (
@@ -13,6 +13,7 @@ const TodoList = ({ todos = [], onRemovePressed }) => (
                 key={todo.text} // Ensure a unique key is provided
                 todo={todo} 
                 onRemovePressed={onRemovePressed} 
+                onCompletedPressed={onCompletedPressed}
             />
         ))}
     </div>
@@ -24,6 +25,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
     onRemovePressed: text => dispatch(removeTodo(text)),
+    onCompletedPressed: text => dispatch(completedTodo(text)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
