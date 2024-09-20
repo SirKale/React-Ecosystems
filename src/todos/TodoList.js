c9bdeb4a867015bdfeacb1807d0c7b23d240d29c
Caleb Carter <calebcarter@Ronalds-MBP.fios-router.home>
2024-09-20 16:35:10 -0400

    Tenth commit. A lot of commits 0_0

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,31 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import TodoListItem from './TodoListItem';
 import NewTodoForm from './NewTodoForm'; 
-import { removeTodo, completedTodo} from "./actions";
+import { completedTodo } from "./actions";
+import { loadTodos, removeTodoRequest, completedTodoRequest } from "./thunks";
 import './TodoList.css'
+import { isLoading } from "./reducers";
 
-const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => (
-    <div className="list-wrapper">
-        <NewTodoForm />
-        {todos.map(todo => (
-            <TodoListItem 
-                key={todo.text} // Ensure a unique key is provided
-                todo={todo} 
-                onRemovePressed={onRemovePressed} 
-                onCompletedPressed={onCompletedPressed}
-            />
-        ))}
-    </div>
-);
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed, isLoading, startLoadingTodos }) => {
+    useEffect(() => {
+        startLoadingTodos();
+    }, [])
+    
+    const loadingMessage = <div>Loading todos...</div>;
+    const content = (
+        <div className="list-wrapper">
+            <NewTodoForm />
+            {todos.map(todo => (
+                <TodoListItem 
+                    key={todo.text} // Ensure a unique key is provided
+                    todo={todo} 
+                    onRemovePressed={onRemovePressed} 
+                    onCompletedPressed={onCompletedPressed}
+                />
+            ))}
+        </div>
+    );
+    return isLoading ? loadingMessage : content;
+};
 
 const mapStateToProps = state => ({
+    isLoading: state.isLoading,
     todos: state.todos,
 });
 
 const mapDispatchToProps = dispatch => ({
-    onRemovePressed: text => dispatch(removeTodo(text)),
-    onCompletedPressed: text => dispatch(completedTodo(text)),
+    startLoadingTodos: () => dispatch(loadTodos()),
+    onRemovePressed: id => dispatch(removeTodoRequest(id)), // This should be defined
+    onCompletedPressed: id => dispatch(completedTodoRequest(id)),
 });
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
